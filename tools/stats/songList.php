9d4872928d15890b9ef0d968c6acc38c20f67ea4
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-03-05 14:46:59 +0000

    bug fix songList.php

--- tools/stats/songList.php
+++ tools/stats/songList.php
@@ -18,34 +18,32 @@
 	include "../../incl/lib/connection.php";
 	require "../../incl/lib/exploitPatch.php";
 	$ep = new exploitPatch();
-	$type = $ep->number($_POST['type']);
-	if ($_POST['name'] == '') {
-		$query = $db->prepare("SELECT ID,name,authorName,size FROM songs WHERE `authorName`='reupload' ORDER BY ID DESC LIMIT 5000");
-		$query->execute();
-		$result = $query->fetchAll();
-		foreach ($result as &$song) {
-			echo "<tr><td>" . $song["ID"] . "</td><td>" . htmlspecialchars($song["name"], ENT_QUOTES) . "</td><td>" . $song['authorName'] . "</td><td>" . $song['size'] . "mb</td></tr>";
-		}
+	if (isset($_POST['type']) == true) {
+		$type = $ep->number($_POST['type']);
 	} else {
-		switch ($type) {
-			case 1:
-				$searchType = "name";
-				break;
-			case 2:
-				$searchType = "authorName";
-				break;
-			default:
-				$searchType = "name";
-				break;
-		}
+		$type = 2;
+	}
+	switch ($type) {
+		case 1:
+			$searchType = "name";
+			break;
+		case 2:
+			$searchType = "authorName";
+			break;
+		default:
+			$searchType = "name";
+			break;
+	}
+	if (isset($_POST['name']) == true) {
 		$name = $ep->remove($_POST['name']);
-		$query = $db->prepare("SELECT ID,name,authorName,size FROM songs WHERE " . $searchType . " LIKE CONCAT('%', :name, '%') ORDER BY ID DESC LIMIT 5000");
-		$query->execute([':name' => $name]);
-
-		$result = $query->fetchAll();
-		foreach ($result as &$song) {
-			echo "<tr><td>" . $song["ID"] . "</td><td>" . htmlspecialchars($song["name"], ENT_QUOTES) . "</td><td>" . $song['authorName'] . "</td><td>" . $song['size'] . "mb</td></tr>";
-		}
+	} else {
+		$name = 'reupload';
+	}
+	$query = $db->prepare("SELECT ID,name,authorName,size FROM songs WHERE " . $searchType . " LIKE CONCAT('%', :name, '%') ORDER BY ID DESC LIMIT 5000");
+	$query->execute([':name' => $name]);
+	$result = $query->fetchAll();
+	foreach ($result as &$song) {
+		echo "<tr><td>" . $song["ID"] . "</td><td>" . htmlspecialchars($song["name"], ENT_QUOTES) . "</td><td>" . $song['authorName'] . "</td><td>" . $song['size'] . "mb</td></tr>";
 	}
 	?>
 </table>
